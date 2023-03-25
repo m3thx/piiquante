6e2d809ab465dfeceb414eef34702918b571c888
m3thx <mathieu.hatstatt@gmail.com>
2023-03-25 13:09:19 +0100

    25.01.23 Ajout path & routes sauces

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
 const app = express();
 
@@ -29,6 +31,8 @@ app.use((req, res, next) => {
 
 //Routes
 app.use('/api/auth', userRoutes)
+app.use('/api/sauces', sauceRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 
 module.exports = app;
