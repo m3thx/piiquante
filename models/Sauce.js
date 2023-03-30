e37eed6cd97a120bb7a096c5a5cdb0e1db8133ae
m3thx <mathieu.hatstatt@gmail.com>
2023-03-30 20:32:52 +0200

    30.03.23 Correction type de champs pour usersLiked et usersDisliked

--- models/Sauce.js
+++ models/Sauce.js
@@ -10,8 +10,8 @@ const sauceSchema = mongoose.Schema({
     heat: { type: Number, min: 1, max: 10 },
     likes: { type: Number, default: 0 },
     dislikes: { type: Number, default: 0 },
-    usersLiked: { type: [ String ] },
-    usersDisliked: { type: [ String ] }
+    usersLiked: { type: [String], default: [] },
+    usersDisliked: { type: [String], default: [] }
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
