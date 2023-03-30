8011ba9a7375963946a9416092db2cddaa5fd154
m3thx <mathieu.hatstatt@gmail.com>
2023-03-30 20:33:25 +0200

    30.03.23 Route pour les likes

--- routes/sauce.js
+++ routes/sauce.js
@@ -10,6 +10,6 @@ router.get('/:id', sauceCtrl.getOneSauce)
 router.post('/', auth, multer, sauceCtrl.createSauce)
 router.put('/:id', auth, multer, sauceCtrl.modifySauce)
 router.delete('/:id', auth, sauceCtrl.deleteSauce)
-// router.post('/:id/like', auth)
+router.post('/:id/like', auth, sauceCtrl.likeSauce)
 
 module.exports = router;
